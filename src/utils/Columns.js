b1cef06b1480863fc66a9b7b8738786ad02d1975
rudrashisdutta <rudra2001official@gmail.com>
2022-04-13 19:18:01 +0530

    Column names Utils has been added
    13-04-2022 07-17p

---
+++ src/utils/Columns.js
@@ -0,0 +1,80 @@
+const columns = [
+    {
+        accessor: "slNo",
+        Header: "Sl. No."
+    },
+    {
+        accessor: "businessCode",
+        Header: "Business Code"
+    },
+    {
+        accessor: "custNumber",
+        Header: "Customer Number"
+    },
+    {
+        accessor: "clearDate",
+        Header: "Clear Date"
+    },
+    {
+        accessor: "businessYear",
+        Header: "Business Year"
+    },
+    {
+        accessor: "docID",
+        Header: "Document ID"
+    },
+    {
+        accessor: "postingDate",
+        Header: "Posting Date"
+    },
+    {
+        accessor: "documentCreateDate",
+        Header: "Document Create Date"
+    },
+    {
+        accessor: "dueInDate",
+        Header: "Due In Date"
+    },
+    {
+        accessor: "invoiceCurrency",
+        Header: "Invoice Currency"
+    },
+    {
+        accessor: "documentType",
+        Header: "Document Type"
+    },
+    {
+        accessor: "postingID",
+        Header: "Posting ID"
+    },
+    {
+        accessor: "totalOpenAmount",
+        Header: "Total Open Amount"
+    },
+    {
+        accessor: "baselineCreateDate",
+        Header: "Baseline Create Date"
+    },
+    {
+        accessor: "custPaymentTerms",
+        Header: "Customer Payment Terms"
+    },
+    {
+        accessor: "invoiceID",
+        Header: "Invoice ID"
+    },
+    {
+        accessor: "isOpen",
+        Header: "Is Open"
+    },
+    {
+        accessor: "predictedDate",
+        Header: "Predicted Date"
+    },
+    {
+        accessor: "ageBucket",
+        Header: "Age Bucket"
+    }
+]
+
+export default columns;
