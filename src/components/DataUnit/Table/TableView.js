80d28515bc78ac462c73be90509c495eb764a548
rudrashisdutta <rudra2001official@gmail.com>
2022-04-14 22:38:51 +0530

    Upgraded UI
    14-04-2022 10-38p

--- src/components/DataUnit/Table/TableView.js
+++ src/components/DataUnit/Table/TableView.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { useEffect, useState, useMemo } from 'react';
 import { useTable, usePagination, useSortBy, useRowSelect } from 'react-table'
 import './TableView.css'
 import COLUMNS from '../../../utils/Columns'
-import { Checkbox, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, Button, ButtonGroup, TextField } from '@mui/material';
+import { Checkbox, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, Button, ButtonGroup, TextField, Popper, Grow, Paper, MenuList, MenuItem, ClickAwayListener  } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ControlPanel from '../../ControlPanel/ControlPanel'
 import axios from 'axios';
 
@@ -59,7 +59,32 @@ function TableView(props) {
           })
       })
 
-      const {pageIndex} = state
+    const {pageIndex} = state
+    const options = [10, 15, 30, 50, 100, 200]
+    const anchorRef = React.useRef(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const [open, setOpen] = React.useState(false);
+    const handleClick = () => {
+        console.info(`You clicked ${options[selectedIndex]}`);
+      };
+    
+      const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setPageSize(options[index])
+        setOpen(false);
+      };
+    
+      const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+      };
+    
+      const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+          return;
+        }
+    
+        setOpen(false);
+      };
 
     return (
         <div className='tableView' align="center">
@@ -97,7 +122,54 @@ function TableView(props) {
                 </Table>
                 </TableContainer>
             <div align='center' className='tableFunctions'>
-            <ButtonGroup size="large" aria-label="large button group" align="center">
+            <ButtonGroup variant="contained" ref={anchorRef} aria-label="split button" align="center">
+                <Button onClick={handleClick}>{options[selectedIndex]}</Button>
+                <Button
+                size="small"
+                aria-controls={open ? 'split-button-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-label="select merge strategy"
+                aria-haspopup="menu"
+                onClick={handleToggle}
+                >
+                <ArrowDropDownIcon />
+                </Button>
+            </ButtonGroup>
+            <Popper
+                open={open}
+                anchorEl={anchorRef.current}
+                role={undefined}
+                transition
+                disablePortal
+            >
+                {({ TransitionProps, placement }) => (
+                <Grow
+                    {...TransitionProps}
+                    style={{
+                    transformOrigin:
+                        placement === 'center top'
+                    }}
+                >
+                    <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                        <MenuList id="split-button-menu" autoFocusItem>
+                        {options.map((option, index) => (
+                            <MenuItem
+                            key={option}
+                            selected={index === selectedIndex}
+                            onClick={(event) => handleMenuItemClick(event, index)}
+                            >
+                            {option}
+                            </MenuItem>
+                        ))}
+                        </MenuList>
+                    </ClickAwayListener>
+                    </Paper>
+                </Grow>
+                )}
+            </Popper>
+            <br/><br/><br/>
+            <ButtonGroup size="large" aria-label="large button group" align="center" className=".buttonGroup">
                 <Button className='Button' variant="outlined" onClick={() =>gotoPage(0)} disabled={!canPreviousPage}><b>ᐊᐊ</b></Button>
                 <Button className='Button' variant="outlined" onClick={previousPage} disabled={!canPreviousPage}><b>ᐊ</b></Button>
                 <span className='message'>
@@ -109,10 +181,10 @@ function TableView(props) {
                 <Button className='Button' variant="outlined" onClick={nextPage} disabled={!canNextPage}><b>ᐅ</b></Button>
                 <Button className='Button' variant="outlined" onClick={() =>gotoPage(pageCount - 1)} disabled={!canNextPage}><b>ᐅᐅ</b></Button>
             </ButtonGroup>
+            
             </div>
         </div>
     );
 }
 
 export default TableView;
-export const a = {tableState}
